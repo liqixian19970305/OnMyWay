ffd016650e725ed9b13280a0390995798f8373be
QIXIAN LI <liqixian19970305@berkeley.edu>
2017-10-06 22:16:32 -0700

    Initial Commit

---
+++ OnMyWay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnMyWay
+//
+//  Created by Tony Li on 10/6/17.
+//  Copyright © 2017 Tony Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
